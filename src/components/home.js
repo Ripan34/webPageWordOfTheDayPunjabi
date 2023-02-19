6f18dfddb7c18e1b2a60c9f8d924acabcb87f15d
ripan34 <ripandeep.singh@sjsu.edu>
2023-02-18 23:02:01 -0800

    added photos, initial

---
+++ src/components/home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from './Home.module.css';
+import sv from './lgR.png';
+import dnd from './dnd.svg';
+import pApp3 from './punApp3.png';
+import pApp4 from './punApp4.png';
+
+const Home = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <img alt="logo" src={sv}/>
+                <h1 >Word of the day <span>Punjabi</span></h1>
+                <div className={styles.desc}>
+                    Learn and  Share 
+                </div>
+                <div className={styles.desc}>
+                    Daily Punjabi words, text extraction from images,
+                </div>
+                <div className={styles.desc}>
+                    and easy sharing and printing options.
+                </div>
+                <a href="https://google.com">
+                    <img alt="download" className={styles.dnd} src={dnd}/>
+                </a>
+                <div className={styles.cpyR}>&copy; COPYRIGHT 2023, Word of the day Punjabi.</div>
+                <a className={styles.priv} href="https://www.privacypolicies.com/live/7d6255bf-0fbe-4523-bf28-ddeaf0547500">Privacy Policy</a>
+            </div>
+            <div className={styles.right}>
+                <img alt="phone" className={styles.pApp1} src={pApp3}/>
+                <img alt="phone" className={styles.pApp4} src={pApp4}/>
+            </div>
+        </div>
+    )
+}
+export default Home;
